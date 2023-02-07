758c57dfba3aabc4d98bd9aa678348822e14c08e
juliechen23 <julie.chen@kellogg.northwestern.edu>
2023-02-07 22:31:50 +0000

    class 6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  resources "tacos" #means someone can visit /tacos
+  resources "dice"
+  resources "cards"
+  resources "companies"
+  resources "contacts"
 end
